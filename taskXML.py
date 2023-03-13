20b0d24665c6e4ff3deeb6cad0543bd332425b2a
Keenan <keenandup87@gmail.com>
2023-03-13 13:56:06 +0200

    issue-5

--- taskXML.py
+++ taskXML.py
@@ -12,14 +12,15 @@ for child in root.iter('movie'):
 for desc in root.iter('description'):
     print(desc.text)
 
-# Count the number of movies that are favourites and not favourites
+# Use a dictionary to count the number of favourites
 fav_count = 0
-not_fav_count = 0
-for movie in root.iter('movie'):
-    if movie.get('favorite') == 'True':
-        fav_count += 1
-    else:
-        not_fav_count += 1
+counts = {'True': 0, 'False': 0}
 
-print("Number of favourite movies:", fav_count)
-print("Number of non-favourite movies:", not_fav_count)
+for movie in root.iter('movie'):
+    is_fav = movie.get('favorite') == 'True'
+    counts[str(is_fav)] += 1
+    if is_fav:
+        fav_count += 1
+
+print("Number of favourite movies:", counts['True'])
+print("Number of non-favourite movies:", counts['False'])
